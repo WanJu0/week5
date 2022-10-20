4bed738e47c44ae2cecf972d6bd7be30dd1cea6f
WanJu Lin <t2roioaui@gmail.com>
2022-10-20 23:17:03 +0800

    init commit

--- data.sql
+++ data.sql
@@ -56,7 +56,9 @@ CREATE TABLE `message` (
   `content` varchar(255) NOT NULL,
   `like_count` int unsigned NOT NULL DEFAULT '0',
   `time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`),
+  KEY `member_id` (`member_id`),
+  CONSTRAINT `message_ibfk_1` FOREIGN KEY (`member_id`) REFERENCES `member` (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb3;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -79,4 +81,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-10-20 10:37:59
+-- Dump completed on 2022-10-20 23:15:09
